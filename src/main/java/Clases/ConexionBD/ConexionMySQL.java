39c615d7bb9427db9b4648443c934db8db66b39e
JohnCh-867 <spartanyt876@gmail.com>
2025-07-12 17:28:29 -0500

    Update ConexionMySQL.java
    nombre de la BD

--- src/main/java/Clases/ConexionBD/ConexionMySQL.java
+++ src/main/java/Clases/ConexionBD/ConexionMySQL.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionMySQL {
-    private static final String CADENA_CONEXION = "jdbc:mysql://localhost:3306/bdcolegio2prueba?useSSL=false&serverTimezone=UTC";
+    private static final String CADENA_CONEXION = "jdbc:mysql://localhost:3306/bdcolegio?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
     private static final String CONTRASEÑA = "123456";
 
